d740f5fcfdaf3a3f9c6f963c8153a580387aef41
Kevin Napoli <kvnnap@gmail.com>
2022-01-20 17:37:06 +0100

    In progress...

--- CandelaDXR/Scene/Scene.cpp
+++ CandelaDXR/Scene/Scene.cpp
@@ -1,11 +1,21 @@
 #include "Scene.h"
 
+#include <stdexcept>
+
 using std::vector;
+using std::string;
 using std::size_t;
+using std::runtime_error;
 
 using candela::scene::Texture;
 using candela::scene::Material;
 using candela::scene::Scene;
+
+Scene::Scene()
+{
+	sceneGraph.NodeName = "_root_";
+	sceneGraph.Transform = DirectX::XMMatrixIdentity();
+}
 
 const vector<Texture>& Scene::getTextures() const
 {
@@ -26,5 +36,34 @@ const vector<Material>& Scene::getMaterials() const
 void Scene::addMaterial(Material material)
 {
 	materials.push_back(std::move(material));
+
+}
+
+void Scene::startGroup(const string& name)
+{
+	if (!currentGroupName.empty())
+		endGroup();
+	if (spanDataMap.find(name) != spanDataMap.end())
+		throw runtime_error("Scene group " + string(name) + " already exists");
+	spanDataMap[name] = { name, indexData.size(), 0 };
+}
+
+void Scene::endGroup()
+{
+	if (currentGroupName.empty())
+		return;
+	// Transfer vertices from collission map to buffers
+	// TODO
+	auto& index = spanDataMap[currentGroupName];
+	index.Size = indexData.size() - index.Start;
+	currentGroupName.clear();
+}
+
+void candela::scene::Scene::addFace(
+	const std::array<mathematics::Vector3, 3>& pos, 
+	const std::array<mathematics::Vector2, 3>& tex,
+	const std::array<mathematics::Vector3, 3>& norm,
+	std::uint32_t materialId)
+{
 
 }
--- CandelaDXR/Scene/Scene.h
+++ CandelaDXR/Scene/Scene.h
@@ -2,6 +2,10 @@
 
 #include <cstdint>
 #include <vector>
+#include <string>
+#include <array>
+#include <map>
+#include <unordered_map>
 
 #include "Texture.h"
 #include "Material.h"
@@ -9,11 +13,42 @@
 
 namespace candela::scene
 {
+	struct SceneNode
+	{
+		// Connectivity
+		std::unique_ptr<SceneNode> Parent;
+		std::vector<std::unique_ptr<SceneNode>> Children;
+
+		// Data
+		mathematics::Matrix Transform;
+		std::string NodeName;
+		std::string GroupName;
+	};
+
+	struct IndexedSpan
+	{
+		std::string Name;
+		std::size_t Start;
+		std::size_t Size;
+	};
+
+	struct alignas(16) FaceAttributes
+	{
+		std::uint32_t materialId;
+		std::uint32_t areaLightId;
+	};
+
+	struct alignas(16) AreaLight {
+		DirectX::XMVECTOR intensity;
+		std::uint32_t instanceIndex;
+		std::uint32_t primitiveId;
+		std::uint32_t materialId;
+	};
+
 	class Scene
 	{
 	public:
-		//const Texture& getTexture(std::size_t id) const;
-		//std::size_t getNumberOfTextures() const;
+		Scene();
 
 		const std::vector<Texture>& getTextures() const;
 		std::size_t addTexture(Texture texture);
@@ -21,13 +56,39 @@ namespace candela::scene
 		const std::vector<Material>& getMaterials() const;
 		void addMaterial(Material texture);
 
+		void startGroup(const std::string& name);
+		void endGroup();
+		void addFace(const std::array<mathematics::Vector3, 3> &pos,
+					 const std::array<mathematics::Vector2, 3> &tex, 
+					 const std::array<mathematics::Vector3, 3> &norm, 
+					 std::uint32_t materialId);
 	private:
+		// Methods
+		//void addVertex(mathematics::Vector3 pos, mathematics::Vector2 tex, mathematics::Vector3 normal);
+
+		// Data
 		std::vector<Texture> textures;
 		std::vector<Material> materials;
+		std::vector<AreaLight> lights;
+
+		// These contain the vertices. The 3 arrays must all be the same size
+		// Storing separately vs interleaved. Trying separate first.
 		std::vector<mathematics::Vector3> vertices;
 		std::vector<mathematics::Vector2> textureCoords;
 		std::vector<mathematics::Vector3> normals;
 
+		// Global index data - these indices refer to the above arrays
 		std::vector<int> indexData;
+
+		// Group data - Divides index data into sections that make up the meshes
+		std::unordered_map<std::string, IndexedSpan> spanDataMap;
+
+		std::map<std::array<float, 8>, int> collisionMap;
+
+		// Groups for the index data - must connect with scene graph
+		SceneNode sceneGraph;
+
+		// Temp data
+		std::string currentGroupName;
 	};
 }
